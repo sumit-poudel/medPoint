98adf02d60015744ff67ed67bf31db17efaf1b3d
sumitpoudel <sumitpoudel325@gmail.com>
2025-08-17 17:52:32 +0545

    back to php -_-

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # medPoint
-a express server based site that uses ajax to dynamically generate elements received from the server
+a php server based site that uses ajax to dynamically generate elements received from the server
--- index.php
+++ index.php
@@ -3,23 +3,30 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-  <title>medPoint</title>
+     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>  
+    <style type="text/tailwindcss" >
+     @theme {
+  --color-med-lime: #28A745;
+  --color-txt-ash: #666666;
+  --color-bdr-ash: #CECECE;
+  --color-med-drklime: #304D30;
+}
+    </style>
 </head>
 <body>
      <header class=" w-full py-2 flex items-center justify-around border-b-bdr-ash border-b-2 " >
-        <img src="med-logo.png" class="h-8" alt="medpoint logo"> 
+        <img src="./public/med-logo.png" class="h-8" alt="medpoint logo"> 
       <section class="flex items-center rounded-md border-2 border-r-0 h-10 p-0.5 pl-2 border-bdr-ash overflow-visible " >
            <label for="search">Search |</label>
         <input type="text" id="search" placeholder="search" value="" class=" focus:outlime-0 pl-1 h-10 text-txt-ash" >
       <button type="search" class=" active:bg-med-drklime rounded-md w-10.75 h-10.75 flex justify-center items-center  bg-med-lime text-white font-extrabold  " >
-          <img class="h-4 w-4 " src="search.svg" alt="search">
+          <img class="h-4 w-4 " src="./public/search.svg" alt="search">
         </button>
       </section>
         <section>
         <button onclick="window.location.href='/login' " type="login" class="flex items-center px-2 h-10 gap-1 border-2 rounded-md border-bdr-ash active:bg-txt-ash active:text-white " >
           login
-          <img class="h-7 w-7" src="login-svg.svg" alt="login">
+          <img class="h-7 w-7" src="./public/login-svg.svg" alt="login">
         </button>
       </section>
     </header>
@@ -29,8 +36,10 @@
       <button type="options" >diabetes care</button>
       <button type="options" >digestive health</button>
     </nav>
-    <main>
+    <main class=" flex h-40 justify-center items-center " >
+                  <h3>
                to be continue...
+</h3>
     </main>
     <footer class="bg-med-lime text-white">
       <div class="max-w-7xl mx-auto px-6 py-10 grid grid-cols-5  gap-8 border-b " >
