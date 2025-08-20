366f900e13cc90e3206ce745994e2a9a68222d95
sumitpoudel <sumitpoudel325@gmail.com>
2025-08-20 08:59:02 +0545

    sperate header footer

---
+++ footer.php
@@ -0,0 +1,82 @@
+  <footer class="bg-med-drklime text-white">
+    <div class="max-w-7xl mx-auto px-6 py-10 grid grid-cols-5  gap-8 border-b ">
+
+
+      <div>
+        <h3 class="font-semibold  mb-3.5">Contact</h3>
+        <p class=" gap-2.5 mb-2">
+          <img src="./public/location.svg" class="h-4 w-4 inline-block " alt="location"> Ramailo-Chowk, Chitwan
+        </p>
+        <p class="gap-2.5 mb-2">
+          <img src="./public/mail.svg" class="h-4 w-4 inline-block " alt="mail"> sumitpoudel@gmail.com
+          <img src="./public/mail.svg" class="h-4 w-4 inline-block " alt="mail"> bimalmagar@gmail.com
+        </p>
+
+      </div>
+
+
+      <div>
+        <h3 class="font-semibold  mb-3.5">Quick Links</h3>
+        <ul class="space-y-2">
+          <li><a href="index.php" class="hover:underline">Home</a> </li>
+          <li><a href="login.php" class="hover:underline">login</a> </li>
+          <li><a href="#" class="hover:underline">Services</a> </li>
+          <li><a href="#" class="hover:underline">Careers</a> </li>
+          <li><a href="#" class="hover:underline">Contact</a> </li>
+        </ul>
+
+      </div>
+
+      <div>
+        <h3 class="font-semibold mt-0.5 mb-3.5">Specialities</h3>
+        <ul class="space-y-2">
+          <li>Anesthesiology </li>
+          <li>Psychiatry</a> </li>
+          <li>General surgery </li>
+          <li>Family medicine </li>
+          <li>Pediatrics </li>
+        </ul>
+
+      </div>
+
+
+
+      <div>
+        <h3 class="font-semibold  mb-3.5">Services</h3>
+        <ul class="space-y-2">
+          <li>Medical</li>
+          <li>Operation </li>
+          <li>Laboratory </li>
+          <li>ICU</li>
+          <li>Patient Ward</li>
+        </ul>
+
+      </div>
+
+
+      <div>
+        <h3 class="font-semibold  mb-3.5">Social Media</h3>
+        <div class="flex gap-4 text-2xl">
+          <a href="#" class="hover:text-blue-400">🌐</a>
+          <a href="#" class="hover:text-pink-500">📸</a>
+          <a href="#" class="hover:text-blue-400"> 💼</a>
+          <a href="#" class="hover:text-red-600"> ▶</a>
+        </div>
+
+      </div>
+    </div>
+
+
+    <div class="max-w-7xl mx-auto px-6 py-4 flex flex-col md:flex-row justify-between text-gray-300 text-sm">
+      <p>© 2023 HEALTHY. All rights reserved.</p>
+      <div class="flex gap-6  mt-0">
+        <a href="#" class="hover:underline">Terms and Conditions</a>
+        <a href="#" class="hover:underline">Privacy Policy</a>
+      </div>
+    </div>
+  </footer>
+  <script src="./index.js">
+  </script>
+  </body>
+
+  </html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  <style type="text/tailwindcss">
+    @theme {
+  --color-med-lime: #28A745;
+  --color-txt-ash: #666666;
+  --color-bdr-ash: #CECECE;
+  --color-med-drklime: #304D30;
+}
+    </style>
+</head>
